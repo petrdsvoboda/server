2504f663ace8403b9d8cff7e4e58ada9cb400c4e
Petr D. Svoboda <petrdsvoboda@gmail.com>
2020-08-21 15:49:15 +0200

    Export server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Controller } from './models/Controller'
 export { default as Router } from './models/Router'
+export { serve, ServerOptions } from './server'
 export { default as CacheService } from './services/db/CacheService'
 export { default as DBService } from './services/db/DBService'
 export { default as QueryService } from './services/db/QueryService'
